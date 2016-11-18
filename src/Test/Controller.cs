fd8b9e194b64e80dfe1be138f90160977cd58d95
Anton Efimenko <reloni@ya.ru>
2016-11-18 23:37:11 +0300

    Initial

---
+++ src/Test/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test
+{
+	public class HomeController : Controller
+	{
+		public JsonResult Time()
+		{
+			return Json(new { Type = "Time", Value = DateTime.Now.ToString("hh:mm:ss tt") });
+		}
+
+		public JsonResult Year()
+		{
+			return Json(new { Type = "Year", Value = DateTime.Now.Year });
+		}
+	}
+}
